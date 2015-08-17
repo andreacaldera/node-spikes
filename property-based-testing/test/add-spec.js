d4327dafa41434c6210102d1985377a6aea1a907
Andrea Caldera <calderaa@visa.com>
2015-08-17 15:43:18 +0100

    adding test

--- property-based-testing/test/add-spec.js
+++ property-based-testing/test/add-spec.js
@@ -1,9 +1,22 @@
 var expect = require("chai").expect;
+var jsc = require("jsverify");
+
 var add = require('../add').add;
 
 describe("Add", function() {
-	it("should add two numbers", function() {
+
+	it("should add two fixed numbers", function() {
 		expect(add(1, 1)).to.equal(2);		
+    });
+
+    it("should add two random numbers", function() {
+
+    	var randomAdd = jsc.forall("integer", "integer", function (x, y) {
+    		return add(x, y) === add(y, x);
+  		});
+
+  		jsc.assert(randomAdd);
+
     });
 
 });
